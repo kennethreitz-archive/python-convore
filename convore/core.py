ce9f0c468824af73910a446de724b40aff962a7b
Kenneth Reitz <me@kennethreitz.com>
2011-02-13 13:05:36 -0500

    That's one way to do it..

--- convore/core.py
+++ convore/core.py
@@ -21,41 +21,15 @@ __copyright__ = 'Copyright 2011 Kenneth Reitz'
 
 
 class Convore(object):
-	"""The :class:`Convore` object is the heart of this api wrapper. It provides all core
-    functionality.
-
-    # Usually you create a :class:`Dataset` instance in your main module, and append
-    #    rows and columns as you collect data. ::
-    # 
-    #        data = tablib.Dataset()
-    #        data.headers = ('name', 'age')
-    # 
-    #        for (name, age) in some_collector():
-    #            data.append((name, age))
-    # 
-    #    You can also set rows and headers upon instantiation. This is useful if dealing
-    #    with dozens or hundres of :class:`Dataset` objects. ::
-    # 
-    #        headers = ('first_name', 'last_name')
-    #        data = [('John', 'Adams'), ('George', 'Washington')]
-    # 
-    #        data = tablib.Dataset(*data, headers=headers)
-
+	"""The :class:`Convore` object is the heart of this api wrapper. It
+	provides all core functionality.
 
     :param username: Username to authenticate with.
     :param password: Password for given username.
-
-
-    # .. admonition:: Format Attributes Definition
-    # 
-    #   If you look at the code, the various output/import formats are not
-    #   defined within the :class:`Dataset` object. To add support for a new format, see
-    #   :ref:`Adding New Formats <newformats>`.
-
+    
 	"""
 
 	def __init__(self, username, password):
-		
 		
 		self.verify()
 
