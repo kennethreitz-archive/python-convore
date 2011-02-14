92ba2d40da214bfe4df1a5f3553c5ef1457b96b9
Kenneth Reitz <me@kennethreitz.com>
2011-02-14 03:03:57 -0500

    no need for that

--- convore/request.py
+++ convore/request.py
@@ -30,17 +30,5 @@ class ConvoreRequest(object):
 		self.username = username
 		self.password = password
 
-	def get(self, *path_components):
-		path_components = filter(None, path_components)
-
-	def post(self, *path_components, **extra_post_data):
-		return self.make_request("/".join(path_components), extra_post_data,
-			method="POST")
-
-	def make_request(self, path, extra_post_data=None, method="GET"):
-		extra_post_data = extra_post_data or {}
-		url = "/".join([self.url_prefix, path])
-		return self.raw_request(url, extra_post_data, method=method)
-
 class ConvoreError(Exception):
 	"""An error occured while making a request to the Convore API."""
