4c9fbe50a1111f349a8356df9ae60a27526453e4
Kenneth Reitz <me@kennethreitz.com>
2011-02-21 15:04:32 -0500

    setup.py test

--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ from distutils.core import setup
 
 
 def publish():
-	"""Publish to PyPi"""
-	os.system("python setup.py sdist upload")
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
 
 if sys.argv[-1] == "publish":
-	publish()
-	sys.exit()
+    publish()
+    sys.exit()
+
+elif sys.argv[-1] == "test":
+    import tox
+    tox.cmdline()
 
 required = ['requests']
 
@@ -21,27 +25,27 @@ if sys.version_info > (2,6):
     required.append('simplejson')
 
 setup(
-	name='convore',
-	version='0.0.1',
-	description='Python wrapper for the Convore API (non-streaming).',
-	long_description=open('README.rst').read() + '\n\n' +
-	                 open('HISTORY.rst').read(),
-	author='Kenneth Reitz',
-	author_email='me@kennethreitz.com',
-	url='https://github.com/kennethreitz/python-convore',
-	packages= ['convore'],
-	install_requires=required,
-	license='ISC',
-	classifiers=(
-		# 'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: ISC License (ISCL)',
-		'Programming Language :: Python',
+    name='convore',
+    version='0.0.1',
+    description='Python wrapper for the Convore API (non-streaming).',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/python-convore',
+    packages= ['convore'],
+    install_requires=required,
+    license='ISC',
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		# 'Programming Language :: Python :: 3.0',
-		# 'Programming Language :: Python :: 3.1',
-	),
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
 )
